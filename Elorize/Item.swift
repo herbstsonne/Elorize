fc8f1b79e09c935b5036b3eb06531c812f73cdb6
miri <mirisasse@gmail.com>
2026-01-27 23:52:18 +0100

    Initial Commit

---
+++ Elorize/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Elorize
+//
+//  Created by Miriam Sasse on 27.01.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ElorizeTests/ElorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElorizeTests.swift
+//  ElorizeTests
+//
+//  Created by Miriam Sasse on 27.01.26.
+//
+
+import Testing
+@testable import Elorize
+
+struct ElorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
