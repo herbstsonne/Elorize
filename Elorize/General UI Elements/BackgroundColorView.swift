adf999c8597b0cfc0c518cdf1bfa5068499cacc7
Miriam <57719825+herbstsonne@users.noreply.github.com>
2026-05-13 16:28:18 +0200

    New features (#39)

--- Elorize/General UI Elements/BackgroundColorView.swift
+++ Elorize/General UI Elements/BackgroundColorView.swift
@@ -6,7 +6,7 @@ struct BackgroundColorView: View {
 		ZStack {
 			// Deep, inky background with a subtle vignette
 			LinearGradient(
-				colors: [Color.app(.background_primary), Color.app(.button_default)],
+				colors: [Color.app(.background_primary), Color.app(.gold_primary)],
 				startPoint: .topLeading,
 				endPoint: .bottomTrailing
 			)
